0cfd286bf0e652ee59509c00f43db8b21656614a
monk-codes <your_donotreply_email_id>
2024-08-14 16:24:41 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Mentor from "./components/Mentor";
 
 const App = () => {
  return (
-  <div className="min-h-screen bg-zinc-700">
-   <h1 className="text-2xl font-bold text-center p-2">Book my Mentor📚</h1>
-   <div className=" flex flex-col px-12 py-2 gap-2 ">
+  <div className="min-h-screen bg-zinc-700 items-center">
+   <h1 className="text-2xl font-bold text-center p-2 ">Book my Mentor📚</h1>
+   <div className=" flex flex-col px-12 py-2 gap-2">
     <Mentor />
     <Booking />
    </div>
